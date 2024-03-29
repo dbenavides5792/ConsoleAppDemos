4a3ad6e040a916653bcb3407caf54c0973db22a6
dbena <dbena@DESKTOP-U6KTTB7>
2024-03-29 14:11:16 -0700

    Add project files.

---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Different Data Types
+/*
+    text - string
+    integer - int
+    decimal - double, float, decimal
+    logical - bool
+ */
+
+string name = "Happy Feet";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name); // Strring concatenation
+Console.WriteLine($"They call me {name}"); // String Interpolation
+Console.WriteLine("I was given the name {0}", name); // Formatted string
+
+int age = 31;
+int retirementYearsLeft = 11;
+int retirementAge = age + retirementYearsLeft;
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+bool isRetired = false;
+Console.WriteLine("Am I retired? " + isRetired);
---
+++ OutputAppDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// WriteLine is a method that takes a block of text and print it to the screen
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I am from Mexico!");
+Console.WriteLine("ANything you want to say");
+
+// WriteLine is a method that takes a block of text and print it to the screen
+// It does not break the line after it prints the text
+Console.Write("Line 1");
+Console.Write("Line 2");
